49a0f72f0f5a8c77765b58678388fa878de00d08
Rafael Felipe <rafafelipesilva@gmail.com>
2020-04-26 21:05:53 -0300

    add & list transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,30 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('Type of transaction is invalid');
+    }
+
+    if (Number.isNaN(Number(value))) {
+      throw Error('Value of transaction is invalid');
+    }
+
+    if (type === 'outcome') {
+      const getBalance = this.transactionsRepository.getBalance();
+
+      if (getBalance.total < value) {
+        throw Error('You dont have money enough');
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
